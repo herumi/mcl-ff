ee9fb39b1ad0014290b916c7a06ecc1b2c43578c
MITSUNARI Shigeo <herumi@nifty.com>
2023-03-14 09:22:05 +0900

    first commit

---
+++ gen_ff.py
@@ -0,0 +1,122 @@
+from s_xbyak_llvm import *
+import argparse
+
+unit = 0
+unit2 = 0
+
+def setGlobalParam(u):
+  global unit, unit2
+  unit = u
+  unit2 = u * 2
+
+def gen_add(N):
+  bit = unit * N
+  resetGlobalIdx()
+  Int(unit)
+  pz = IntPtr(unit)
+  px = IntPtr(unit)
+  py = IntPtr(unit)
+  name = f'mcl_fp_addPre{N}'
+  with Function(name, Void, pz, px, py):
+    x = zext(loadN(px, N), bit + unit)
+    y = zext(loadN(py, N), bit + unit)
+    z = add(x, y)
+    storeN(trunc(z, bit), pz)
+    r = trunc(lshr(z, bit), unit)
+    ret(Void)
+
+def gen_mulUU():
+  resetGlobalIdx();
+  z = Int(unit2)
+  x = Int(unit)
+  y = Int(unit)
+  name = f'mul{unit}x{unit}L'
+  with Function(name, z, x, y, private=True) as f:
+    x = zext(x, unit2)
+    y = zext(y, unit2)
+    z = mul(x, y)
+    ret(z)
+  return f
+
+def gen_extractHigh():
+  resetGlobalIdx()
+  z = Int(unit)
+  x = Int(unit2)
+  name = f'extractHigh{unit}'
+  with Function(name, z, x, private=True):
+    x = lshr(x, unit)
+    z = trunc(x, unit)
+    ret(z)
+
+def gen_mulPos(mulUU):
+  resetGlobalIdx()
+  xy = Int(unit2)
+  px = IntPtr(unit)
+  y = Int(unit)
+  i = Int(unit)
+  name = f'mulPos{unit}x{unit}'
+  with Function(name, xy, px, y, i, private=True):
+    x = load(getelementptr(px, i))
+    xy = call(mulUU, x, y)
+    ret(xy)
+
+def gen_once():
+  mulUU = gen_mulUU()
+  gen_extractHigh()
+  gen_mulPos(mulUU)
+
+def gen_mcl_fp_add(N, isFullBit=True):
+  bit = unit * N
+  resetGlobalIdx();
+  pz = IntPtr(unit)
+  px = IntPtr(unit)
+  py = IntPtr(unit)
+  pp = IntPtr(unit)
+  name = 'mcl_fp_add'
+  if not isFullBit:
+    name += 'NF'
+  name += f'{N}L'
+  with Function(name, Void, pz, px, py, pp):
+    x = loadN(px, N)
+    y = loadN(py, N)
+    if isFullBit:
+      x = zext(x, bit + unit)
+      y = zext(y, bit + unit)
+      x = add(x, y)
+      p = loadN(pp, N)
+      p = zext(p, bit + unit)
+      y = sub(x, p)
+      c = trunc(lshr(y, bit), 1)
+      x = select(c, x, y)
+      x = trunc(x, bit)
+      storeN(x, pz)
+    else:
+      x = add(x, y)
+      p = loadN(pp, N)
+      y = sub(x, p)
+      c = trunc(lshr(y, bit - 1), 1)
+      x = select(c, x, y)
+      storeN(x, pz)
+    ret(Void)
+
+def main():
+  parser = argparse.ArgumentParser(description='gen bint')
+  parser.add_argument('-u', type=int, default=64, help='unit')
+  parser.add_argument('-n', type=int, default=0, help='max size of unit')
+  parser.add_argument('-proto', action='store_true', default=False, help='show prototype')
+  parser.add_argument('-addn', type=int, default=0, help='mad size of add/sub')
+  opt = parser.parse_args()
+  if opt.n == 0:
+    opt.n = 9 if opt.u == 64 else 17
+    opt.addn = 16 if opt.u == 64 else 32
+
+  setGlobalParam(opt.u)
+  if opt.proto:
+    showPrototype()
+
+  gen_once()
+  gen_mcl_fp_add(3, isFullBit=False)
+  term()
+
+if __name__ == '__main__':
+  main()
---
+++ s_xbyak_llvm.py
@@ -0,0 +1,322 @@
+VOID_TYPE = 0
+INT_TYPE = 1
+IMM_TYPE = 2
+INT_PTR_TYPE = 3
+
+eq = 1
+neq = 2
+ugt = 3
+uge = 4
+ult = 5
+ule = 6
+sgt = 7
+sge = 8
+slt = 9
+sle = 10
+
+g_showPrototype = False
+g_text = []
+g_undefLabel = {}
+g_defLabelN = 1
+g_undefLabelN = 1
+g_globalIdx = 0
+
+def output(s):
+  g_text.append(s)
+
+def getLine():
+  return len(g_text)
+
+def init():
+  global g_text
+  g_text = []
+
+def showPrototype():
+  global g_showPrototype
+  g_showPrototype = True
+
+class Label:
+  def __init__(self):
+    self.n = 0
+  def __str__(self):
+    if self.n > 0:
+      return getDefLabel(self.n)
+    global g_undefLabel
+    global g_undefLabelN
+    if -self.n in g_undefLabel:
+      g_undefLabel[-self.n].append(getLine())
+    else:
+      self.n = -g_undefLabelN
+      g_undefLabelN += 1
+      g_undefLabel.setdefault(-self.n, []).append(getLine())
+    return getUndefLabel(-self.n)
+
+def L(label):
+  if type(label) != Label:
+    raise Exception(f'bad type {label}')
+  if label.n > 0:
+    raise Exception(f'already defined {label}')
+  lines = []
+  if label.n < 0:
+    global g_undefLabelN
+    n = -label.n
+    if n in g_undefLabel:
+      lines = g_undefLabel[n]
+      oldStr = getUndefLabel(n)
+      del g_undefLabel[n]
+  global g_defLabelN
+  label.n = g_defLabelN
+  g_defLabelN += 1
+  if lines:
+    newStr = getDefLabel(label.n)
+    global g_text
+    for line in lines:
+      g_text[line] = g_text[line].replace(oldStr, newStr)
+  output(f'{getDefLabel(label.n)}:')
+
+class Function:
+  def __init__(self, name, ret, *args, private=False, noalias=True):
+    self.name = name
+    self.ret = ret
+    self.args = args
+    self.private = private
+    self.noalias = noalias
+    if g_showPrototype and not private:
+      s = f'{ret.getCtype()} {name}('
+      for i in range(len(args)):
+        if i > 0:
+          s += ', '
+        s += args[i].getCtype()
+      s += ');'
+      print(s)
+      return
+
+    s = 'define '
+    if private:
+      s += 'private '
+    s += f'{ret.getType()} @{name}('
+    for i in range(len(args)):
+      if i > 0:
+        s += ', '
+      s += args[i].getFullName(noalias)
+    s += ')'
+    output(s)
+    output('{')
+
+  def getName(self):
+    return f'{self.ret.getType()} @{self.name}'
+
+  def close(self):
+    output('}')
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, ex_type, ex_value, trace):
+    self.close()
+
+def genFunc(name):
+  def f(*args):
+    if not args:
+      return output(name)
+    s = ''
+    for arg in args:
+      if s != '':
+        s += ', '
+      if g_gas:
+        if type(arg) == int:
+          s += str(arg)
+        else:
+          s += str(arg)
+      else:
+        s += str(arg)
+    return output(name + ' ' + s)
+  return f
+
+def CondTypeToStr(t):
+  tbl = [
+    "eq", "neq", "ugt", "uge", "ult", "ule", "sgt", "sge", "slt", "sle"
+  ]
+  return tbl[t-1]
+
+def getGlobalIdx():
+  global g_globalIdx
+  g_globalIdx += 1
+  return g_globalIdx
+
+def resetGlobalIdx():
+  global g_globalIdx
+  g_globalIdx = 0
+
+class Operand:
+  def __init__(self, t, bit, imm=0):
+    self.t = t
+    self.bit = bit
+    self.imm = imm
+    if t != IMM_TYPE:
+      self.idx = getGlobalIdx()
+
+  def getFullName(self, noalias=False):
+    return f'{self.getType(noalias)} {self.getName()}'
+
+  def getType(self, noalias=False):
+    if self.t == INT_TYPE or self.t == IMM_TYPE:
+      return f'i{self.bit}'
+    if self.t == INT_PTR_TYPE:
+      if noalias:
+        return f'i{self.bit}* noalias'
+      else:
+        return f'i{self.bit}*'
+    if self.t == VOID_TYPE:
+      return 'void'
+    raise Exception('no type')
+
+  def getCtype(self):
+    if self.t == INT_TYPE:
+      return f'uint{self.bit}_t'
+    if self.t == INT_PTR_TYPE:
+      return f'uint{self.bit}_t*'
+    if self.t == VOID_TYPE:
+      return 'void'
+    raise Exception('no C type')
+
+  def getName(self):
+    if self.t == INT_TYPE or self.t == INT_PTR_TYPE:
+      return f'%r{self.idx}'
+    if self.t == IMM_TYPE:
+      return str(self.imm)
+    return ''
+
+class Int(Operand):
+  def __init__(self, bit):
+    self = Operand.__init__(self, INT_TYPE, bit)
+
+class IntPtr(Operand):
+  def __init__(self, bit):
+    self = Operand.__init__(self, INT_PTR_TYPE, bit)
+
+class Imm(Operand):
+  def __init__(self, imm):
+    bit = int(imm).bit_length()
+    bit = ((bit + 31) // 32) * 32
+    if bit == 0:
+      bit = 32
+    self = Operand.__init__(self, IMM_TYPE, bit, imm)
+
+Void = Operand(VOID_TYPE, 0)
+
+def term():
+  if g_showPrototype:
+    return
+  n = len(g_text)
+  i = 0
+  while i < n:
+    s = g_text[i]
+    print(s)
+    i += 1
+
+# r = op x, v
+
+def genOp_r_x_v(name):
+  def f(x, v):
+    if type(v) == int:
+      v = Imm(v)
+    r = Int(x.bit)
+    output(f'{r.getName()} = {name} {x.getFullName()}, {v.getName()}')
+    return r
+  return f
+
+tbl = ['lshr', 'ashr', 'shl', 'add', 'sub', 'mul', 'and_', 'or_']
+for name in tbl:
+  llvmName = name.strip('_')
+  globals()[name] = genOp_r_x_v(llvmName)
+
+def select(cond, x, y):
+  r = Int(x.bit)
+  output(f'{r.getName()} = select {cond.getFullName()}, {x.getFullName()}, {y.getFullName()}')
+  return r
+
+# r = op x to y
+def zext(x, bit):
+  r = Int(bit)
+  output(f'{r.getName()} = zext {x.getFullName()} to {r.getType()}')
+  return r
+
+def trunc(x, bit):
+  r = Int(bit)
+  output(f'{r.getName()} = trunc {x.getFullName()} to {r.getType()}')
+  return r
+
+def bitcast(x, bit):
+  r = IntPtr(bit)
+  output(f'{r.getName()} = bitcast {x.getFullName()} to {r.getType()}')
+  return r
+
+
+
+# op x
+def ret(x):
+  output(f'ret {x.getFullName()}')
+
+# op x, v
+def store(x, v):
+  output(f'store {x.getFullName()}, {v.getFullName()}')
+
+# r = op x
+def load(x):
+  r = Int(x.bit)
+  output(f'{r.getName()} = load {r.getType()}, {x.getFullName()}')
+  return r
+
+def getelementptr(x, v):
+  if type(v) == int:
+    v = Imm(v)
+  r = IntPtr(x.bit)
+  output(f'{r.getName()} = getelementptr i{x.bit}, {x.getFullName()}, {v.getFullName()}')
+  return r
+
+def call(func, *args):
+  s = ''
+  if func.ret.t != VOID_TYPE:
+    r = Operand(func.ret.t, func.ret.bit)
+    s = f'{r.getName()} = '
+  s += f'call {func.getName()}('
+  for i in range(len(args)):
+    if i > 0:
+      s += ', '
+    s += args[i].getFullName()
+  s += ')'
+  output(s)
+  if func.ret.t != VOID_TYPE:
+    return r
+
+####
+
+def loadN(p, n, offset=0):
+  if offset != 0:
+    p = getelementptr(p, offset)
+  v = load(p)
+  unit = v.bit
+  for i in range(1, n):
+    v = zext(v, v.bit + unit)
+    t = load(getelementptr(p, i))
+    t = zext(t, v.bit)
+    t = shl(t, unit * i)
+    v = or_(v, t)
+  return v
+
+def storeN(r, p, offset=0):
+  if offset != 0:
+    p = getelementptr(p, offset)
+  unit = p.bit
+  if r.bit == unit:
+    store(r, p)
+    return
+  n = r.bit // unit
+  for i in range(n):
+    pp = getelementptr(p, i)
+    t = trunc(r, unit)
+    store(t, pp)
+    if i < n-1:
+      r = lshr(r, unit)
+
